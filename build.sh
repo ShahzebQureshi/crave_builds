bb8909e94fa79580eb7ae2ee8630842fd0cdf1e3
ShahzebQureshi <55016172+ShahzebQureshi@users.noreply.github.com>
2026-04-24 01:05:44 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,45 +1,28 @@
 #!/bin/bash
 rm -rf .repo/local_manifests
-repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+repo init --no-repo-verify --git-lfs -u https://github.com/yaap/manifest -b sixteen -g default,-mips,-darwin,-notdefault
 /opt/crave/resync.sh
 
 # Clean old dirs
-rm -rf out/target/product/guacamole
-rm -rf device/oneplus/guacamole
+rm -rf out/target/product/hotdogg
+rm -rf device/oneplus/hotdogg
 rm -rf device/oneplus/sm8150-common
 rm -rf kernel/oneplus/sm8150
-rm -rf vendor/oneplus/guacamole
+rm -rf vendor/oneplus/hotdogg
 rm -rf vendor/oneplus/sm8150-common
 rm -rf hardware/oneplus
 rm -rf vendor/infinity-priv/keys
-rm -rf vendor/qcom/opensource/core-utils
-rm -rf device/qcom/common
-rm -rf vendor/qcom/common
-rm -rf packages/apps/KProfiles
-rm -rf hardware/qcom/display
-rm -rf hardware/qcom/media
-rm -rf hardware/qcom/audio
-rm -rf device/qcom/sepolicy_vndr
-
 # Clone sources
-git clone https://github.com/ShahzebQureshi/device_oneplus_guacamole -b sixteen-infinity device/oneplus/guacamole --depth=1
-git clone https://github.com/ShahzebQureshi/device_oneplus_sm8150-common -b sixteen device/oneplus/sm8150-common --depth=1
+git clone https://github.com/ShahzebQureshi/device_oneplus_hotdogg -b sixteen-test device/oneplus/hotdogg --depth=1
+git clone https://github.com/yaap/device_oneplus_sm8150-common -b sixteen device/oneplus/sm8150-common --depth=1
 git clone https://github.com/yaap/kernel_oneplus_sm8150 -b sixteen kernel/oneplus/sm8150 --depth=1
-git clone https://github.com/yaap/vendor_oneplus_guacamole -b sixteen vendor/oneplus/guacamole --depth=1
+git clone https://github.com/yaap/vendor_oneplus_hotdog -b sixteen vendor/oneplus/hotdogg --depth=1
 git clone https://github.com/yaap/vendor_oneplus_sm8150-common -b sixteen vendor/oneplus/sm8150-common --depth=1
 git clone https://github.com/ShahzebQureshi/ak vendor/lineage-priv/keys --depth=1
-git clone https://github.com/yaap/vendor_qcom_opensource_core-utils -b sixteen vendor/qcom/opensource/core-utils --depth=1
-git clone https://github.com/yaap/device_qcom_common -b sixteen device/qcom/common --depth=1
-git clone https://gitlab.com/yaosp/vendor_qcom_common -b sixteen vendor/qcom/common --depth=1
-git clone https://github.com/yaap/packages_apps_KProfiles -b sixteen packages/apps/KProfiles --depth=1
-git clone https://github.com/LineageOS/android_hardware_qcom_sm8150_display -b lineage-23.2 hardware/qcom/display --depth=1
-git clone https://github.com/yaap/hardware_qcom-caf_sm8150_media -b sixteen hardware/qcom/media --depth=1
-git clone https://github.com/yaap/hardware_qcom-caf_sm8150_audio -b sixteen hardware/qcom/audio --depth=1
-
-
-git clone https://github.com/LineageOS/android_device_qcom_sepolicy_vndr -b lineage-23.2-legacy-um device/qcom/sepolicy_vndr --depth=1
-# Clone hardware LAST
 git clone https://github.com/LineageOS/android_hardware_oneplus hardware/oneplus --depth=1
+
+
+
 
 
 # Set up build environment
@@ -49,9 +32,7 @@ export TZ="Asia/Karachi"
 
 
 # Build
-export TARGET_INIT_VENDOR_LIB=""
-export TARGET_SURFACEFLINGER_UDFPS_LIB=""
 . build/envsetup.sh
 export SKIP_ABI_CHECKS=true
-lunch infinity_guacamole-userdebug
+lunch yaap_hotdogg-userdebug
 m bacon
