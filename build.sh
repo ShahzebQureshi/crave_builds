2a38d7e33ed2080ceb5fe52ee7f8e00f8a01586c
ShahzebQureshi <55016172+ShahzebQureshi@users.noreply.github.com>
2026-04-26 04:55:20 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,46 +11,23 @@ rm -rf kernel/oneplus/sm8150
 rm -rf vendor/oneplus/guacamole
 rm -rf vendor/oneplus/sm8150-common
 rm -rf hardware/oneplus
-rm -rf vendor/infinity-priv/keys
-rm -rf vendor/qcom/opensource/core-utils
-rm -rf device/qcom/common
-rm -rf vendor/qcom/common
-rm -rf packages/apps/KProfiles
-rm -rf hardware/qcom/display
-rm -rf hardware/qcom/media
-rm -rf hardware/qcom/audio
-rm -rf device/qcom/sepolicy_vndr
 
 # Clone sources
-git clone https://github.com/ShahzebQureshi/device_oneplus_guacamole -b sixteen-infinity device/oneplus/guacamole --depth=1
-git clone https://github.com/ShahzebQureshi/device_oneplus_sm8150-common -b sixteen device/oneplus/sm8150-common --depth=1
-git clone https://github.com/yaap/kernel_oneplus_sm8150 -b sixteen kernel/oneplus/sm8150 --depth=1
-git clone https://github.com/yaap/vendor_oneplus_guacamole -b sixteen vendor/oneplus/guacamole --depth=1
-git clone https://github.com/yaap/vendor_oneplus_sm8150-common -b sixteen vendor/oneplus/sm8150-common --depth=1
-git clone https://github.com/ShahzebQureshi/ak vendor/lineage-priv/keys --depth=1
-git clone https://github.com/yaap/vendor_qcom_opensource_core-utils -b sixteen vendor/qcom/opensource/core-utils --depth=1
-git clone https://github.com/yaap/device_qcom_common -b sixteen device/qcom/common --depth=1
-git clone https://gitlab.com/yaosp/vendor_qcom_common -b sixteen vendor/qcom/common --depth=1
-git clone https://github.com/yaap/packages_apps_KProfiles -b sixteen packages/apps/KProfiles --depth=1
-git clone https://github.com/LineageOS/android_hardware_qcom_sm8150_display -b lineage-23.2 hardware/qcom/display --depth=1
-git clone https://github.com/yaap/hardware_qcom-caf_sm8150_media -b sixteen hardware/qcom/media --depth=1
-git clone https://github.com/yaap/hardware_qcom-caf_sm8150_audio -b sixteen hardware/qcom/audio --depth=1
+git clone https://github.com/ShahzebQureshi/device_oneplus_guacamole-Evox -b infinity device/oneplus/guacamole --depth=1
+git clone https://github.com/Codecity001/device_oneplus_sm8150-common-1 device/oneplus/sm8150-common --depth=1
+git clone https://github.com/Codecity001/android_kernel_oneplus_sm8150 kernel/oneplus/sm8150 --depth=1
+git clone https://github.com/ShahzebQureshi/proprietary_vendor_oneplus_guacamole -b infinity vendor/oneplus/guacamole --depth=1
+git clone https://github.com/Codecity001/vendor_oneplus_sm8150-common-1 vendor/oneplus/sm8150-common --depth=1
 
-
-git clone https://github.com/LineageOS/android_device_qcom_sepolicy_vndr -b lineage-23.2-legacy-um device/qcom/sepolicy_vndr --depth=1
 # Clone hardware LAST
-git clone https://github.com/LineageOS/android_hardware_oneplus hardware/oneplus --depth=1
-
+git clone https://github.com/Codecity001/hardware_oneplus-1 hardware/oneplus --depth=1
 
 # Set up build environment
 export BUILD_USERNAME=ShahzebQureshi
 export BUILD_HOSTNAME=Linux
 export TZ="Asia/Karachi"
 
-
 # Build
-export TARGET_INIT_VENDOR_LIB=""
-export TARGET_SURFACEFLINGER_UDFPS_LIB=""
 . build/envsetup.sh
 export SKIP_ABI_CHECKS=true
 lunch infinity_guacamole-userdebug
